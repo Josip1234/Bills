0352572f557fecaeef7f8c704e3ca1085871bd31
Josip Bošnjak <jbosnjak3@gmail.com>
2024-04-16 20:26:57 +0200

    update

--- bills.sql
+++ bills.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2024 at 07:38 PM
+-- Generation Time: Apr 15, 2024 at 02:02 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -45,7 +45,8 @@ INSERT INTO `articles` (`id`, `serial_num`, `article_name`, `article_price`, `ku
 (3, '20002', 'MAJICA', 12.9, NULL, NULL),
 (4, '004440', 'DVD Miami Vice - The Complete Series (32 discs) (ENG)', 51.63, NULL, NULL),
 (5, '004199', 'Poštarina Tisak', 2.92, NULL, NULL),
-(6, 'rdx_9996', 'MERCEDES BENZ G-CLA ', 4.95, NULL, NULL);
+(6, 'rdx_9996', 'MERCEDES BENZ G-CLA ', 4.95, NULL, NULL),
+(7, 'R25 11888', 'MONSTER ENERGY 0,5L', 1.86, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -73,7 +74,8 @@ INSERT INTO `bill_footer` (`id`, `bill_number`, `date`, `ZKI`, `JIR`, `ref_numbe
 (1, '26691/3205/60', '2022-07-14 14:57:07', '61190da357423eb32fe0a6b39ae19111', '8a0bd979-8948-4668-8030-3685a45aa06f', '83/2320560208', 'Kupujte i online na www.konzum.hr\r\n', '', '62226620908'),
 (2, '3033/PP4/1', '2024-04-06 10:24:26', '6867931bd51a0836a61a495a37ec5314', 'fe2cade0-e86a-4188-958a-ec4580d98a09', '0.15522042769493574', 'ozn. operatera: OPERATER 48\nNaćin plaćanja: gotovina\nHvala na kupnji! Zamjena robe moguća je u roku od 10 dana od danje kupnje uz predočenje računa. Odjeća i galenterija mora sadržavati pripadajuće deklaracije, a obuća originalnu ambalažu. Obuća, odjeća i galenterija ne smije biti nošena u slučaju zamjene. U slučaju zamjene robe povrat novca nije moguć', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\WIN_20230224_19_01_22_Pro.jpg', '39314924844'),
 (4, '186/POSL1/1', '2024-03-11 11:37:00', 'no zki number', 'no jir number', 'no ref number', '{\r\n   \"Details\":{\r\n      \"Dospjijeće\":\"29.02.2024.\",\r\n      \"Način/mjesto isporuke\":\"Tisak (913070)\",\r\n      \"Način plaćanja\":\"Transakcijski račun\",\r\n      \"Poziv na broj\":\"186-2024\",\r\n      \"Kupac\":\"Bošnjak Josip (Tisak) Sveti rok 81 34000 Požega\",\r\n      \"Tel./e-mail\":\"0919759754, jbosnjak3@gmail.com\"\r\n   }\r\n}', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\WIN_20240407_14_15_18_Pro.jpg', '88317003800'),
-(5, '15411/918390/1', '2024-04-09 15:44:00', '1b6849e061551a54b69141fcd63ddcd0', '9756af73-3c7a-4ea0-80eb-1dbd9f5a11a4', NULL, 'Blagajna:1, spr:1190, PM:918390, GOTOVINA', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\WIN_20240409_19_30_32_Pro.jpg', '32497003047');
+(5, '15411/918390/1', '2024-04-09 15:44:00', '1b6849e061551a54b69141fcd63ddcd0', '9756af73-3c7a-4ea0-80eb-1dbd9f5a11a4', NULL, 'Blagajna:1, spr:1190, PM:918390, GOTOVINA', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\WIN_20240409_19_30_32_Pro.jpg', '32497003047'),
+(6, '15707/P01/1', '2024-04-15 10:19:39', 'f750b53b04516f5dd8f16887c574347c', '6732c045-605b-4c37-9acb-1dbd9f5aa907', NULL, 'Ana Đurina \r\nPožega', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\2024-04-15 13_48_48-Window.png', '67618415844');
 
 -- --------------------------------------------------------
 
@@ -103,7 +105,8 @@ INSERT INTO `bill_item` (`id`, `bill_number`, `type`, `article_name`, `shop_ssn`
 (3, '3033/PP4/1', 'Shopping bill', 'MAJICA', '39314924844', 1, NULL, NULL, NULL, NULL),
 (4, '186/POSL1/1', 'Electronic bill', 'DVD Miami Vice - The Complete Series (32 discs) (ENG)', '88317003800', 1, NULL, NULL, NULL, NULL),
 (5, '186/POSL1/1', 'Electronic bill', 'Poštarina Tisak', '88317003800', 1, NULL, NULL, NULL, NULL),
-(6, '15411/918390/1', 'Shopping bill', 'MERCEDES BENZ G-CLA ', '32497003047', 1, NULL, NULL, NULL, '{\"Details\":{\r\n        \"PDV\":\"13%\"\r\n}\r\n}');
+(6, '15411/918390/1', 'Shopping bill', 'MERCEDES BENZ G-CLA ', '32497003047', 1, NULL, NULL, NULL, '{\"Details\":{\r\n        \"PDV\":\"13%\"\r\n}\r\n}'),
+(7, '15707/P01/1', 'Shopping bill', 'MONSTER ENERGY 0,5L', '67618415844', 1, NULL, NULL, 0.5, '{\"Details\":{\r\n\"Način plaćanja\":\"Gotovina/novčanice\",\r\n\"PDV\":\"25%\"\r\n}\r\n}');
 
 -- --------------------------------------------------------
 
@@ -147,7 +150,8 @@ CREATE TABLE `other_shop_or_bill_details` (
 INSERT INTO `other_shop_or_bill_details` (`id`, `bill_number`, `details`, `shop_ssn`) VALUES
 (2, '26691/3205/60', '{\"Details\":{\r\n      \"Blagajna\":60,\r\n      \"Blagajnik\":\"KE\"\r\n}\r\n}', '62226620908'),
 (3, '3033/PP4/1', '{\r\n   \"Details\":{\r\n      \"Oznaka operatera\":\"OPERATER 48\",\r\n      \"Način plaćanja\":\"Gotovina\"\r\n   }\r\n}', '39314924844'),
-(4, NULL, '{\r\n   \"Details\":{\r\n      \"VAT ID\":\"HR88317003800\",\r\n      \"IBAN\":\"HR5923400091160415211\",\r\n      \"SWIFT\":\"PBZGHR2X\",\r\n      \"Paypal\":\"paypal@crovortex.com\",\r\n\"Oznaka operatera\":\"1\"\r\n   }\r\n}', '88317003800');
+(4, NULL, '{\r\n   \"Details\":{\r\n      \"VAT ID\":\"HR88317003800\",\r\n      \"IBAN\":\"HR5923400091160415211\",\r\n      \"SWIFT\":\"PBZGHR2X\",\r\n      \"Paypal\":\"paypal@crovortex.com\",\r\n\"Oznaka operatera\":\"1\"\r\n   }\r\n}', '88317003800'),
+(5, NULL, '{\"Details\":{\r\n\"Informacijski sustav\":\"(c) THOR inf.sustav\",\r\n\"Web adresa\":\"www.monri.com\"\r\n}\r\n}', '67618415844');
 
 -- --------------------------------------------------------
 
@@ -165,6 +169,7 @@ CREATE TABLE `shop` (
 --
 
 INSERT INTO `shop` (`id`, `shop_name`) VALUES
+(5, 'ASKA d.o.o'),
 (3, 'Crovortex, obrt za trgovinu'),
 (1, 'KONZUM plus d.o.o.'),
 (2, 'T.O. Koala Vl. M.Jelic'),
@@ -197,7 +202,8 @@ INSERT INTO `shop_detail` (`id`, `shop_name`, `address`, `ssn`, `shop number`, `
 (1, 'KONZUM plus d.o.o.', 'ZAGREB, NOVA VES 17', '62226620908', '3205', '0800 400 000', '', '', 'Zagreb, Ulica Marijana Čavića 1A', 'https://www.konzum.hr/'),
 (2, 'T.O. Koala Vl. M.Jelic', 'Matice Hrvatske 4, Požega', '39314924844', 'Izdv. Pogon 4', '034 / 275 – 990', NULL, NULL, 'Sl.Graničara 29, Nova Gradiška', 'https://koala-shop.hr/'),
 (3, 'Crovortex, obrt za trgovinu', 'Cenkovečka 5 10000 Zagreb, Croatia', '88317003800', 'POSL1', '+385 (0)91/508-3664', NULL, 'crovortex@gmail.com', NULL, 'https://www.crovortex.com/'),
-(4, 'TISAK plus d.o.o', 'SL. BROD - AUTOB. KOLODVOR', '32497003047', '918390', '+385 1 2641 111', '+385 1 2641 500', 'tisak@tisak.hr', 'SLAVONSKA AV. 11a, Zagreb', 'https://www.tisak.hr/');
+(4, 'TISAK plus d.o.o', 'SL. BROD - AUTOB. KOLODVOR', '32497003047', '918390', '+385 1 2641 111', '+385 1 2641 500', 'tisak@tisak.hr', 'SLAVONSKA AV. 11a, Zagreb', 'https://www.tisak.hr/'),
+(5, 'ASKA d.o.o', 'HR-34000 POŽEGA, Svatog Roka 38\r\n', '67618415844', 'Prodavaonica br.1', '091/300-1036', NULL, 'uprava@aska.hr', 'HR-31400 Đakovo, Psunjska 1A', NULL);
 
 -- --------------------------------------------------------
 
@@ -220,7 +226,8 @@ INSERT INTO `shop_logo` (`id`, `shop_name`, `logo1_url`, `logo2_url`) VALUES
 (1, 'KONZUM plus d.o.o.', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\konzum_logo.png', ''),
 (2, 'T.O. Koala Vl. M.Jelic', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\koala_logo.png', ''),
 (3, 'Crovortex, obrt za trgovinu', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\crovertex_logo.png', ''),
-(4, 'TISAK plus d.o.o', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\tisak_logo.png', '');
+(4, 'TISAK plus d.o.o', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\tisak_logo.png', ''),
+(5, 'ASKA d.o.o', 'C:\\Users\\Korisnik\\Desktop\\xmp\\htdocs\\Bills\\aska_logo.png', '');
 
 -- --------------------------------------------------------
 
@@ -255,7 +262,8 @@ INSERT INTO `transaction` (`id`, `bill_number`, `shop_ssn`, `serial_num`, `sum_e
 (3, '3033/PP4/1', '39314924844', '20002', 12.9, NULL, NULL, 10.32, 2.58, 12.9, NULL, NULL, NULL, NULL, '{\r\n   \"Details\":{\r\n      \"Oznaka operatera\":\"OPERATER 48\",\r\n      \"Način plaćanja\":\"Gotovina\"\r\n   }\r\n}'),
 (4, '186/POSL1/1', '88317003800', '004440', 51.63, NULL, NULL, 51.63, NULL, 51.63, NULL, NULL, NULL, NULL, NULL),
 (5, '186/POSL1/1', '88317003800', '004199', 2.92, NULL, NULL, 2.92, NULL, 2.92, NULL, NULL, NULL, NULL, NULL),
-(6, '15411/918390/1', '32497003047', 'rdx_9996', 4.95, NULL, NULL, 4.38, 0.57, 4.95, NULL, NULL, NULL, NULL, NULL);
+(6, '15411/918390/1', '32497003047', 'rdx_9996', 4.95, NULL, NULL, 4.38, 0.57, 4.95, NULL, NULL, NULL, NULL, NULL),
+(7, '15707/P01/1', '67618415844', 'R25 11888', 1.86, NULL, 'R25', 1.43, 0.36, 1.86, 0.07, 1, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -372,19 +380,19 @@ ALTER TABLE `type_off_bill`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `bill_footer`
 --
 ALTER TABLE `bill_footer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `bill_item`
 --
 ALTER TABLE `bill_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `multiple_products_sum`
@@ -396,31 +404,31 @@ ALTER TABLE `multiple_products_sum`
 -- AUTO_INCREMENT for table `other_shop_or_bill_details`
 --
 ALTER TABLE `other_shop_or_bill_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `shop`
 --
 ALTER TABLE `shop`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `shop_detail`
 --
 ALTER TABLE `shop_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `shop_logo`
 --
 ALTER TABLE `shop_logo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `type_off_bill`
