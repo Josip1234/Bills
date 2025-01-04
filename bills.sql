067631a28f604c19b337b72688adff1b81eba971
Josip Bošnjak <jbosnjak3@gmail.com>
2025-01-04 14:17:29 +0100

    Update bills.sql

--- bills.sql
+++ bills.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2024 at 10:53 AM
+-- Generation Time: Jan 04, 2025 at 02:15 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
