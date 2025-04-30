ff0283ee4f95e026156f41200a41da0d0ba54b63
Josip Bošnjak <jbosnjak3@gmail.com>
2025-04-30 21:07:14 +0200

    added php scripts and url navigatgion links

--- update_bill.php
+++ update_bill.php
@@ -3,36 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bills</title>
+    <title>Update bill</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="container text-center">
-  <div class="row">
-    <div class="col">
-    <div class="input-group flex-nowrap">
-  <span class="input-group-text" id="addon-wrapping">Search</span>
-  <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="addon-wrapping">
-</div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      <a href="insert_new_bill.php" target="_blank" rel="noopener noreferrer">Insert new bill</a>
-      
-    </div>
-    <div class="col">
-      <a href="update_bill.php" target="_blank" rel="noopener noreferrer"> Update bill</a>
-     
-    </div>
-    <div class="col">
-      <a href="delete_bills.php" target="_blank" rel="noopener noreferrer">Delete bills</a>
-      
-    </div>
-  </div>
-</div>
+    
 </body>
 </html>
