f905408e860f40f58db75a6ac3a6889833e803b3
Josip Bošnjak <jbosnjak3@gmail.com>
2025-05-06 21:01:35 +0200

    update
    when user clicks shop detail now it opens shop details with id passed on with javascript

--- shop.js
+++ shop.js
@@ -1,3 +1,5 @@
 function showShopDetails(id){
-    alert(id);
+    //alert(id);
+    var shop_name=id;
+    window.open("http://localhost/Bills/shop_detail.php?shop_name="+shop_name);
 }
---
+++ shop_detail.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert new shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
+    <script src="shop.js"></script>
+</head>
+<body>
+<div class="container text-center">
+<div class="row">
+    <div class="col">
+    <div class="input-group flex-nowrap">
+  <span class="input-group-text" id="addon-wrapping">Search</span>
+  <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="addon-wrapping">
+</div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <a href="index.php" target="_blank" rel="noopener noreferrer">Back to main page</a>
+      
+    </div>
+  </div>
+
+<div class="row">
+<div class="col">
+    <?php 
+include("functions.php");
+print_all_available_shops();
+
+?>
+</div>
+</div>
+</div>
+</body>
+</html>
